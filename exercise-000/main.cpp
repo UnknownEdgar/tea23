f521115359527230cd61fa8a29d1036a888ee432
Edgar Malinowsky <30591377+UnknownEdgar@users.noreply.github.com>
2025-10-10 11:04:58 +0200

    Added solution part 0

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -10,7 +10,11 @@ int main(int argc, char **argv) {
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+    fmt::print("Hello, {} in version {} !\n", tea::PROJECT_NAME, tea::PROJECT_VER);
+    fmt::println("argc: {}", argc);
+    for(int num = 0; num < argc; num++){
+        fmt::println("argv[{}]: {}", num, argv[num]);
+    };
 
     /* INSERT YOUR CODE HERE */
 
